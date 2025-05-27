c8471fc26339687c99b836db713fcfe2106c8e02
Vaibhav Patel <39291323+vpatel071997@users.noreply.github.com>
2025-05-27 15:41:49 +1000

    adding landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { useEffect, useState } from 'react';
 import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
+import Landing from './components/Landing';
 import URLToQR from './components/URLToQR';
 import VCardGen from './components/VCardGen';
 import VCardView from './components/VCardView';
-import { useEffect, useState } from 'react';
 
 function App() {
   const location = useLocation();
@@ -19,10 +20,10 @@ function App() {
       {showNavBar && (
         <Navbar expand="sm" className="bg-body-tertiary">
           <Container fluid>
-            <Navbar.Brand href="#home">QriCard.</Navbar.Brand>
+            <Navbar.Brand href="/">QriCard.</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse className="justify-content-end" style={{ gap: '1rem' }}>
-              <Nav.Link as={Link} to="/">Url to QR</Nav.Link>
+              <Nav.Link as={Link} to="url-to-qr">Url to QR</Nav.Link>
               <Nav.Link as={Link} to="/vcard-gen">VCard Generator</Nav.Link>
             </Navbar.Collapse>
           </Container>
@@ -30,7 +31,8 @@ function App() {
       )}
       <Container fluid>
         <Routes>
-          <Route path="/" element={<URLToQR />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/url-to-qr" element={<URLToQR />} />
           <Route path="/vcard-gen" element={<VCardGen />} />
           <Route path="/vcard-view" element={<VCardView />} />
         </Routes>
