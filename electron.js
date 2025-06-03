aae7e385d2ac6975741bef59089fbd64b772ac79
Vaibhav Patel <39291323+vpatel071997@users.noreply.github.com>
2025-06-03 13:20:43 +1000

    replaced logo name with icon

--- electron.js
+++ electron.js
@@ -32,7 +32,7 @@ function createWindow() {
   });
 
   if (app.isPackaged) {
-    mainWindow.loadFile(path.join(__dirname, "index.html"));
+    mainWindow.loadFile(path.join(__dirname, "build", "index.html"));
   } else {
     mainWindow.loadURL("http://localhost:3000");
   }
