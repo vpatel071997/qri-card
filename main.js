7fcc41b18635d2718f2a26d8142dea86332ebbdc
Vaibhav Patel <39291323+vpatel071997@users.noreply.github.com>
2025-06-03 11:28:27 +1000

    added electron wrapper

---
+++ main.js
@@ -0,0 +1,78 @@
+const { app, BrowserWindow } = require("electron");
+const { main } = require("framer-motion/client");
+const path = require("path");
+
+let mainWindow;
+let splash;
+
+function createWindow() {
+  // Create the splash screen window
+  splash = new BrowserWindow({
+    width: 400,
+    height: 300,
+    frame: false,
+    alwaysOnTop: true,
+    transparent: false,
+  });
+
+  splash.loadFile(path.join(__dirname, "splash.html"));
+
+  mainWindow = new BrowserWindow({
+    width: 1270,
+    height: 725,
+    frame: true,
+    center: true,
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, "public", "icon.png"),
+    title: "Tapflow.",
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: true,
+    },
+    show: false,
+  });
+
+  if (app.isPackaged) {
+    mainWindow.loadFile(path.join(__dirname, "build", "index.html"));
+  } else {
+    mainWindow.loadURL("http://localhost:3000");
+  }
+
+  function checkReady() {
+    mainWindow.webContents
+      .executeJavaScript("document.readyState")
+      .then((readyState) => {
+        if (readyState === "complete") {
+          splash.destroy();
+          setTimeout(() => {
+            mainWindow.show();
+            mainWindow.reload();
+            mainWindow.focus();
+          }, 1000);
+        } else {
+          setTimeout(checkReady, 1000);
+        }
+      })
+      .catch(() => {
+        setTimeout(checkReady, 1000);
+      });
+  }
+
+  mainWindow.webContents.once("dom-ready", () => {
+    checkReady();
+  });
+
+  mainWindow.on("closed", () => {
+    if (!splash.isDestroyed()) splash.destroy();
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
---
+++ preload.js
@@ -0,0 +1,13 @@
+import { contextBridge, ipcRenderer } from "electron";
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  sendMessage: (channel, data) => {
+    ipcRenderer.send(channel, data);
+  },
+  onMessage: (channel, callback) => {
+    ipcRenderer.on(channel, callback);
+  },
+  removeMessageListener: (channel, callback) => {
+    ipcRenderer.removeListener(channel, callback);
+  },
+});
