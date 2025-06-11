745120cb2433b5d0a31c9ac4b03cb4a2d4630a41
Vaibhav Patel <39291323+vpatel071997@users.noreply.github.com>
2025-06-11 14:06:36 +1000

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,13 +2,21 @@
 
 FILE="src/index.tsx"
 
+OS="$(uname -s)"
+case "${OS}" in
+    Linux*)     SED_OPTIONS="-i" ;;
+    Darwin*)    SED_OPTIONS="-i ''" ;;
+    CYGWIN*|MINGW32*|MSYS*|MINGW*) SED_OPTIONS="-i" ;;
+    *)          echo "Unsupported OS: ${OS}" && exit 1 ;;
+esac
+
 if grep -q 'HashRouter as Router' "$FILE"; then
   # Replace HashRouter with BrowserRouter
-  sed -i '' 's/HashRouter as Router/BrowserRouter as Router/' "$FILE"
+  sed $SED_OPTIONS 's/HashRouter as Router/BrowserRouter as Router/' "$FILE"
   echo "Switched to BrowserRouter"
 elif grep -q 'BrowserRouter as Router' "$FILE"; then
   # Replace BrowserRouter with HashRouter
-  sed -i '' 's/BrowserRouter as Router/HashRouter as Router/' "$FILE"
+  sed $SED_OPTIONS 's/BrowserRouter as Router/HashRouter as Router/' "$FILE"
   echo "Switched to HashRouter"
 else
   echo "No recognizable Router import found."
